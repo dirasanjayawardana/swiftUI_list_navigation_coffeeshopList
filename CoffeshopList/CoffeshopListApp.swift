12064054695f2ffaf07f57acc37c575cfab458e7
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-17 13:08:11 +0700

    Initial Commit

---
+++ CoffeshopList/CoffeshopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeshopListApp.swift
+//  CoffeshopList
+//
+//  Created by MACBOOK PRO on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeshopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
